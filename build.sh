64ccf64a4f677c760402f27b3773404de5bc051d
少鸿 江 <tianxiaoxin001@gmail.com>
2020-03-27 17:27:48 +0800

    hmmm

--- build.sh
+++ build.sh
@@ -10,13 +10,12 @@ echo "build linux package success"
 
 # 开始打包public 文件
 echo "start build frontend"
-cd frontend && echo `dirname $0` && yarn build && cp -r dist ../public
-echo "cp -r dist ../public"
+cd frontend && echo `dirname $0` && yarn build && cp -r dist ../public && echo "cp -r dist ../public"
 
 cd ../
 echo `dirname $0`
 
 echo "start build docker container..."
 # 开始打包docker 文件；然后上传
-docker build -t registry.gitlab.com/honglian/novelweb . && echo "build docker latest image complete!" &&
+docker build -t registry.gitlab.com/honglian/novelweb . && echo "build docker latest image complete!"
 docker push registry.gitlab.com/honglian/novelweb && echo "push docker latest image complete!"
